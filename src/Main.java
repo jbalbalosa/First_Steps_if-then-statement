6577e8e57ba421a5f81067052ebbee86f80efea3
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-17 15:30:24 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Udemy: Java Programming Masterclass");
+        System.out.println("First Steps: if-then-statement");
+
+        boolean isAlien = false;
+        if(!isAlien) { //Simplified from if(isAlien == false)
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scrared of aliens");
+        }
+        else
+            System.out.println("You're an alien");
+
+
+        //Logical Operators
+        int topScore = 80;
+        if (topScore < 100){
+            System.out.println("You got the high score!");
+        }
+        else
+            System.out.println("You got the great score");
+
+/*        int topScore = 90;
+        if (topScore != 100){
+            System.out.println("You didn't get the high score!");
+        }*/
+
+        int secondTopScore = 60;
+        if ((topScore > secondTopScore) && (topScore < 100)){
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+
+
     }
 }
